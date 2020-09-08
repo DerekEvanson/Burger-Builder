d490477b1873f24c334b7e809d59de19de4185ad
Derek Evanson <evanson.derek@gmail.com>
2020-09-08 18:33:10 -0500

    Dynamic Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,34 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	// state = { ingredients: { salad: 1, bacon: 1, cheese: 2, meat: 2 } };
+
+	let ingredientKeys = Object.keys(props.ingredients);
+	//[salad, bacon, cheese, meat]
+
+	//Returns array of arrays of jxs objects with a string type and key
+	let transformedIngredients = ingredientKeys
+		.map((igKey) => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	//console.log(transformedIngredients[1][0]); <BurgerIngredient key='bacon0' type='bacon' />
+	//console.log(transformedIngredients[3][1]); <BurgerIngredient key='meat1' type='meat' />
+	//console.log(transformedIngredients[3][0]); <BurgerIngredient key='meat0' type='meat' />
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients!</p>;
+	}
+
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -26,7 +26,7 @@ class BurgerIngredient extends Component {
 				ingredient = <div className={classes.Cheese}></div>;
 				break;
 			case 'bacon':
-				ingredient = <div className={classes.Meat}></div>;
+				ingredient = <div className={classes.Bacon}></div>;
 				break;
 			case 'salad':
 				ingredient = <div className={classes.Salad}></div>;
