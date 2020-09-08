7cbc9777cf07d5a3a07cac74eba0d71fdb496500
Derek Evanson <evanson.derek@gmail.com>
2020-09-08 10:16:50 -0500

    Layout and Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import Burger from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<Layout>
+					<p>Test</p>
+				</Layout>
+				<Burger />
+			</div>
+		);
 	}
 }
 
