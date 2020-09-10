ddfe6441aa1cc017e26d516185c4accd0354992f
Derek Evanson <evanson.derek@gmail.com>
2020-09-10 09:41:03 -0500

    Navigation Top Bar

--- src/components/Navigation/Toolbar/ToolBar.js
+++ src/components/Navigation/Toolbar/ToolBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import classes from './ToolBar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
 		<Logo />
-		<nav>...</nav>
+		<nav>
+			<NavigationItems />
+		</nav>
 	</header>
 );
 
