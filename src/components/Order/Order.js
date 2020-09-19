f86f5583d0000500e8a00d8fd7a4902fc68d4b7e
Derek Evanson <evanson.derek@gmail.com>
2020-09-18 19:32:16 -0500

    Navigation Links Update

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => (
-	<div>
+	<div className={classes.Order}>
 		<p>Ingredients: Salad (1)</p>
 		<p>
 			Price: <strong>USD 5.45</strong>
