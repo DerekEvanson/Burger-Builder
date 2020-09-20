8507ad5bc3149493958f610c95f34577727befd9
Derek Evanson <evanson.derek@gmail.com>
2020-09-20 12:20:35 -0500

    Forms Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,6 +16,11 @@ class ContactData extends Component {
 					placeholder: 'Your Name',
 				},
 				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+				touched: false,
 			},
 			street: {
 				elementType: 'input',
@@ -24,6 +29,11 @@ class ContactData extends Component {
 					placeholder: 'Street',
 				},
 				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+				touched: false,
 			},
 			zipCode: {
 				elementType: 'input',
@@ -32,6 +42,14 @@ class ContactData extends Component {
 					placeholder: 'ZIP Code',
 				},
 				value: '',
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5,
+					isNumeric: true,
+				},
+				valid: false,
+				touched: false,
 			},
 			country: {
 				elementType: 'input',
@@ -40,6 +58,11 @@ class ContactData extends Component {
 					placeholder: 'Country',
 				},
 				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+				touched: false,
 			},
 			email: {
 				elementType: 'input',
@@ -48,6 +71,12 @@ class ContactData extends Component {
 					placeholder: 'Your E-Mail',
 				},
 				value: '',
+				validation: {
+					required: true,
+					isEmail: true,
+				},
+				valid: false,
+				touched: false,
 			},
 			deliveryMethod: {
 				elementType: 'select',
@@ -58,14 +87,16 @@ class ContactData extends Component {
 					],
 				},
 				value: '',
+				validation: {},
+				valid: true,
 			},
 		},
+		formIsValid: false,
 		loading: false,
 	};
 
 	orderHandler = (event) => {
 		event.preventDefault();
-		alert('You continue!');
 		this.setState({ loading: true });
 		const formData = {};
 		for (let formElementIdentifier in this.state.orderForm) {
@@ -89,14 +120,57 @@ class ContactData extends Component {
 			});
 	};
 
+	checkValidity(value, rules) {
+		let isValid = true;
+		if (!rules) {
+			return true;
+		}
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		return isValid;
+	}
+
 	inputChangedHandler = (event, inputIdentifier) => {
 		const updatedOrderForm = {
 			...this.state.orderForm,
 		};
-		const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier],
+		};
 		updatedFormElement.value = event.target.value;
+		updatedFormElement.valid = this.checkValidity(
+			updatedFormElement.value,
+			updatedFormElement.validation,
+		);
+		updatedFormElement.touched = true;
 		updatedOrderForm[inputIdentifier] = updatedFormElement;
-		this.setState({ orderForm: updatedOrderForm });
+
+		let formIsValid = true;
+		for (let inputIdentifier in updatedOrderForm) {
+			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+		}
+		this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
 	};
 
 	render() {
@@ -107,7 +181,6 @@ class ContactData extends Component {
 				config: this.state.orderForm[key],
 			});
 		}
-
 		let form = (
 			<form onSubmit={this.orderHandler}>
 				{formElementsArray.map((formElement) => (
@@ -116,16 +189,20 @@ class ContactData extends Component {
 						elementType={formElement.config.elementType}
 						elementConfig={formElement.config.elementConfig}
 						value={formElement.config.value}
+						invalid={!formElement.config.valid}
+						shouldValidate={formElement.config.validation}
+						touched={formElement.config.touched}
 						changed={(event) => this.inputChangedHandler(event, formElement.id)}
 					/>
 				))}
-				<Button btnType="Success">ORDER</Button>
+				<Button btnType="Success" disabled={!this.state.formIsValid}>
+					ORDER
+				</Button>
 			</form>
 		);
 		if (this.state.loading) {
 			form = <Spinner />;
 		}
-
 		return (
 			<div className={classes.ContactData}>
 				<h4>Enter your Contact Data</h4>
