3d98679ab55ae4036327d8b1619ea6a108fbd75d
Derek Evanson <evanson.derek@gmail.com>
2020-09-26 14:22:30 -0500

    Orders Redux Refactor

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 	checkoutCancelledHandler = () => {
@@ -16,18 +17,22 @@ class Checkout extends Component {
 
 	render() {
 		let summary = <Redirect to="/" />;
+
 		if (this.props.ings) {
-			<div>
-				<CheckoutSummary
-					ingredients={this.props.ings}
-					checkoutCancelled={this.checkoutCancelledHandler}
-					checkoutContinued={this.checkoutContinuedHandler}
-				/>
-				<Route
-					path={this.props.match.path + '/contact-data'}
-					component={ContactData}
-				/>
-			</div>;
+			const purchasedRedirect = this.purchased ? <Redirect to="/" /> : null;
+			summary = (
+				<div>
+					<CheckoutSummary
+						ingredients={this.props.ings}
+						checkoutCancelled={this.checkoutCancelledHandler}
+						checkoutContinued={this.checkoutContinuedHandler}
+					/>
+					<Route
+						path={this.props.match.path + '/contact-data'}
+						component={ContactData}
+					/>
+				</div>
+			);
 		}
 		return summary;
 	}
@@ -36,6 +41,7 @@ class Checkout extends Component {
 const mapStateToProps = (state) => {
 	return {
 		ings: state.burgerBuilder.ingredients,
+		purchased: state.order.purchased,
 	};
 };
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,10 +3,16 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState = {
 	orders: [],
 	loading: false,
+	purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case actionTypes.PURCHASE_INIT:
+			return {
+				...state,
+				purchased: false,
+			};
 		case actionTypes.PURCHASE_BURGER_START:
 			return {
 				...state,
@@ -20,6 +26,7 @@ const reducer = (state = initialState, action) => {
 			return {
 				...state,
 				loading: false,
+				purchased: true,
 				orders: state.orders.concat(newOrder),
 			};
 		case actionTypes.PURCHASE_BURGER_FAIL:
@@ -27,6 +34,23 @@ const reducer = (state = initialState, action) => {
 				...state,
 				loading: false,
 			};
+		case actionTypes.FETCH_ORDERS_START:
+			return {
+				...state,
+				loading: true,
+			};
+		case actionTypes.FETCH_ORDERS_SUCCESS:
+			return {
+				...state,
+				orders: action.orders,
+				loading: false,
+			};
+		case actionTypes.FETCH_ORDERS_FAIL:
+			return {
+				...state,
+				loading: false,
+			};
+
 		default:
 			return state;
 	}
