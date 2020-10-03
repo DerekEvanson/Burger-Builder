6fb2f84dc094b6d3cee4029e4403e87ada4b10d4
Derek Evanson <evanson.derek@gmail.com>
2020-10-02 23:55:48 -0500

    Shared Functions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
 	state = {
@@ -49,50 +50,17 @@ class Auth extends Component {
 		}
 	}
 
-	checkValidity(value, rules) {
-		let isValid = true;
-		if (!rules) {
-			return true;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		if (rules.isEmail) {
-			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-			isValid = pattern.test(value) && isValid;
-		}
-
-		if (rules.isNumeric) {
-			const pattern = /^\d+$/;
-			isValid = pattern.test(value) && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangedHandler = (event, controlName) => {
-		const updatedControls = {
-			...this.state.controls,
-			[controlName]: {
-				...this.state.controls[controlName],
+		const updatedControls = updateObject(this.state.controls, {
+			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
-				valid: this.checkValidity(
+				valid: checkValidity(
 					event.target.value,
 					this.state.controls[controlName].validation,
 				),
 				touched: true,
-			},
-		};
+			}),
+		});
 		this.setState({ controls: updatedControls });
 	};
 
@@ -104,6 +72,7 @@ class Auth extends Component {
 			this.state.isSignup,
 		);
 	};
+
 	switchAuthModeHandler = () => {
 		this.setState((prevState) => {
 			return { isSignup: !prevState.isSignup };
@@ -143,7 +112,6 @@ class Auth extends Component {
 		}
 
 		let authRedirect = null;
-
 		if (this.props.isAuthenticated) {
 			authRedirect = <Redirect to={this.props.authRedirectPath} />;
 		}
@@ -157,7 +125,7 @@ class Auth extends Component {
 					<Button btnType="Success">SUBMIT</Button>
 				</form>
 				<Button clicked={this.switchAuthModeHandler} btnType="Danger">
-					SWITCH TO {this.state.isSignup ? 'SIGN IN' : 'SIGN UP'}
+					SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}
 				</Button>
 			</div>
 		);
